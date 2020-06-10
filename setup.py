dfe780f9b026cf615fb2c9a7fbbd0a931df13ee7
Jing He <hejing01@qiyi.com>
2020-06-10 16:49:55 +0800

    Release v0.0 into pypi

--- setup.py
+++ setup.py
@@ -1,9 +1,17 @@
+from pathlib import Path
 from setuptools import setup
+
+this_dir = Path(__file__).parent.resolve()
+readme_path = this_dir / 'README.md'
+with readme_path.open('r', encoding='utf-8') as f:
+    long_description = f.read()
 
 setup(name='rconfig',
       version='0.0',
       description='Python config',
       install_requires=['addict'],
+      long_description=long_description,
+      long_description_content_type='text/markdown',
       author='rivergold',
       author_email='jinghe.rivergold@gmail.com',
       license='MIT')
